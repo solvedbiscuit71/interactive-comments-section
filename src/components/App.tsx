b66243ce7038d67b8b7366640e1f93ed039788a9
Praveen Perumal <ppraveen98841@gmail.com>
2022-04-05 08:20:05 +0530

    Completed Fetching and Type Checking the data from 'data.json'

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import UserContext from '../contexts/UserContext';
+import DataProps from '../interfaces/DataProps';
 
 const App:React.FC = () => {
+  const [data, setData] = useState<DataProps | null>(null);
+  useEffect(() => {
+    fetch('data.json')
+      .then(res => res.json())
+      .then(res => setData(res))
+      .catch(err => console.log(err))
+  }, []);
+
   return (
-    <></>
+    <>
+      {
+        data &&
+        <UserContext.Provider value={data.currentUser}>
+        </UserContext.Provider>
+      }
+    </>
   );
 }
 
