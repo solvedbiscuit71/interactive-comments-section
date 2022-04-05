bb5d5a4f47630a6ee5ef9ec9995f02ef1145b908
Praveen Perumal <ppraveen98841@gmail.com>
2022-04-05 09:14:52 +0530

    Dynamically change content if comment/reply is made by currentUser

--- src/components/cards/Comment.tsx
+++ src/components/cards/Comment.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UserContext from '../../contexts/UserContext';
 import { CommentProps } from '../../interfaces/DataProps';
 
 const Comment:React.FC<CommentProps> = (props) => {
+  const currentUser = useContext(UserContext)
+
   return (
     <div className='comment'>
       <div>
         <div>
           <img src={props.user.image.webp} alt={`${props.user.username}'s avatar`} />
           <h2>{props.user.username}</h2>
+          { currentUser.username === props.user.username && <span>YOU</span>}
           <span>{props.createdAt}</span>
         </div>
         <p>{props.content}</p>
@@ -17,10 +21,24 @@ const Comment:React.FC<CommentProps> = (props) => {
         <span>{props.score}</span>
         <img src="images/icon-minus.svg" alt="minus icon" />
       </div>
-      <div>
-        <img src="images/icon-reply.svg" alt="reply icon" />
-        <span>Reply</span>
-      </div>
+      {
+        currentUser.username === props.user.username ?
+        <div>
+          <div>
+            <img src="images/icon-delete.svg" alt="delete icon" />
+            <span>Delete</span>
+          </div>
+          <div>
+            <img src="images/icon-edit.svg" alt="edit icon" />
+            <span>Edit</span>
+          </div>
+        </div>
+        :
+        <div>
+          <img src="images/icon-reply.svg" alt="reply icon" />
+          <span>Reply</span>
+        </div>
+      }
     </div>
   );
 }
--- src/components/cards/Reply.tsx
+++ src/components/cards/Reply.tsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UserContext from '../../contexts/UserContext';
 import { ReplyProps } from '../../interfaces/DataProps';
 
 const Reply:React.FC<ReplyProps> = (props) => {
+  const currentUser = useContext(UserContext)
+
   return (
     <div className='reply'>
       <div>
         <div>
           <img src={props.user.image.webp} alt={`${props.user.username}'s avatar`} />
           <h2>{props.user.username}</h2>
+          { currentUser.username === props.user.username && <span>YOU</span>}
           <span>{props.createdAt}</span>
         </div>
         <p>
@@ -20,10 +24,24 @@ const Reply:React.FC<ReplyProps> = (props) => {
         <span>{props.score}</span>
         <img src="images/icon-minus.svg" alt="minus icon" />
       </div>
-      <div>
-        <img src="images/icon-reply.svg" alt="reply icon" />
-        <span>Reply</span>
-      </div>
+      {
+        currentUser.username === props.user.username ?
+        <div>
+          <div>
+            <img src="images/icon-delete.svg" alt="delete icon" />
+            <span>Delete</span>
+          </div>
+          <div>
+            <img src="images/icon-edit.svg" alt="edit icon" />
+            <span>Edit</span>
+          </div>
+        </div>
+        :
+        <div>
+          <img src="images/icon-reply.svg" alt="reply icon" />
+          <span>Reply</span>
+        </div>
+      }
     </div>
   );
 }
